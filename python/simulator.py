550dfeb1a2fc5815bad8e4ca2fdbf81fc7b55e95
Steve Gattuso <stevenleeg@gmail.com>
2014-02-13 09:54:41 -0500

    Code cleanup

--- python/simulator.py
+++ python/simulator.py
@@ -2,18 +2,23 @@ from board import Board
 import argparse, datetime
 
 parser = argparse.ArgumentParser(description = "RITtaire3D simulator")
-parser.add_argument("-n", metavar="N", dest="n", type=int, 
-        help="Runs one simulation with board size n")
+parser.add_argument("-n", metavar="N", dest="n", type=int,
+    help="Runs one simulation with board size n")
 parser.add_argument("-o", dest="save_image", action="store_true",
-        help="Outputs a graphical representation of the board (saves to output.png)")
-parser.add_argument("-s", metavar="N", dest="start", type=int, 
-        help="Begin simulated range at this board size (inclusive).")
-parser.add_argument("-e", metavar="N", dest="end", type=int, 
-        help="End simulated range at this board size (inclusive).")
-parser.add_argument("-t", metavar="t", dest="simulations", type=int, 
-        help="Simulate this range t times.")
+    help="Outputs a graphical representation of the board (to output.png)")
+parser.add_argument("-s", metavar="N", dest="start", type=int,
+    help="Begin simulated range at this board size (inclusive).")
+parser.add_argument("-e", metavar="N", dest="end", type=int,
+    help="End simulated range at this board size (inclusive).")
+parser.add_argument("-t", metavar="t", dest="simulations", type=int,
+    help="Simulate this range t times.")
 
-def main():   
+def main():
+    """
+    Main function for the simulator. This parses the command line arguments
+    and runs through the simulations, calculating statistics afterwards.
+    """
+    # Parse arguments
     args = parser.parse_args()
     if args.start != None and args.end != None:
         n_start = args.start
@@ -30,6 +35,7 @@ def main():
         parser.print_help()
         return
 
+    # Benchmarks
     total_turns = 0
     start_dt = datetime.datetime.now()
     nturns = {}
@@ -37,19 +43,21 @@ def main():
     if n_start == n_end:
         print "Running size n=%d for %d simulations" % (n_start, simulations)
     else:
-        print "Running range %d to %d for %d simulations..." % (n_start, n_end, simulations)
+        print "Running range %d to %d for %d simulations..." % (
+            n_start, n_end, simulations)
     print
 
+    # Run throught the simulations
     for simulation in range(0, simulations):
         for board_size in range(n_start, n_end + 1):
-            b = Board(board_size)
+            board = Board(board_size)
 
-            b.simulate()
+            board.simulate()
 
-            turns = b.getTurns()
+            turns = board.getTurns()
             total_turns += turns
             if args.save_image:
-                image = b.renderImage()
+                image = board.renderImage()
                 image.save("output.png")
 
             if board_size in nturns:
@@ -58,13 +66,18 @@ def main():
                 nturns[board_size] = turns
 
         print "Finished simulation %d" % simulation
-    
-    for n, total in nturns.items():
-        print "\nAvg for %d is %f" % (n, float(total) / float(simulations))
 
+    # Calculate averages
+    for board_size, total in nturns.items():
+        print "\nAvg for %d is %f" % (
+            board_size, float(total) / float(simulations))
+
+    # More benchmarks
     end_dt = datetime.datetime.now()
-    delta = end_dt - start_dt 
-    print "Simulation completed in %ds with %d turns (%f turns/s)" % (delta.seconds, total_turns, (delta.microseconds * 1000000) / float(total_turns))
+    delta = end_dt - start_dt
+    print "Simulation completed in %ds with %d turns (%f turns/s)" % (
+        delta.seconds, total_turns,
+        (delta.microseconds * 1000000) / float(total_turns))
 
 if __name__ == "__main__":
     main()
