6fd80e5a51fd8ecc8464905b5b40ac8328e460f3
Steve Gattuso <stevenleeg@gmail.com>
2014-02-18 00:24:48 -0500

    That was a nasty little bug...

--- python/board.py
+++ python/board.py
@@ -35,7 +35,8 @@ class Board:
             for y in range(-1, 2):
                 for z in range(-1, 2):
                     if x == 0 and y == 0 and z == 0:
-                        break
+                        continue
+
                     self.directions.append((x, y, z))
 
     def roll(self):
